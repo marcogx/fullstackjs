f6200a6d25c5ee652e40b9a6f9febecef8c502f7
gxiong <guang.xiong@hds.com>
2017-10-06 16:09:04 -0700

    full stack JS

---
+++ src/doggy.test.js
@@ -0,0 +1,6 @@
+import Dog from './doggy'
+
+test('Dog.bark', () => {
+  const testDog = new Dog('test')
+  expect(testDog.bark()).toBe('I am test')
+})
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+
+// const Dog = require('./doggy')
+import Dog from './doggy'
+
+const sherry = new Dog('sherry')
+console.log('====================================')
+console.log(sherry.bark())
+console.log('====================================')
